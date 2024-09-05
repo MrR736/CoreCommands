c539f09d960063b9c7a738733cf73231b31fac49
MrR736 <mrr6451@outlook.com>
2024-09-04 17:20:59 -0700

    Update A.cmd

--- UpdateCore/A.cmd
+++ UpdateCore/A.cmd
@@ -1,22 +1,22 @@
 @echo off
-cd "%root%"
+cd "%ProgramData%\PhoenixOS"
 mkdir Update
-cd %root%\Update
+cd %ProgramData%\PhoenixOS\Update
 wget https://github.com/MrR736/CoreUpdates/archive/refs/heads/main.zip
-7za x main.zip -o%root%\Update
-del /s /q "%root%\Update\main.zip"
+7za x main.zip -o%ProgramData%\PhoenixOS\Update
+del /s /q "%ProgramData%\PhoenixOS\Update\main.zip"
 wget https://github.com/MrR736/CoreSettings/archive/refs/heads/main.zip
-7za x main.zip -o%root%\Update
-del /s /q "%root%\Update\main.zip"
+7za x main.zip -o%ProgramData%\PhoenixOS\Update
+del /s /q "%ProgramData%\PhoenixOS\Update\main.zip"
 wget https://github.com/MrR736/CoreControls/archive/refs/heads/main.zip
-7za x main.zip -o%root%\Update
-del /s /q "%root%\Update\main.zip"
+7za x main.zip -o%ProgramData%\PhoenixOS\Update
+del /s /q "%ProgramData%\PhoenixOS\Update\main.zip"
 wget https://github.com/MrR736/CoreCommands/archive/refs/heads/main.zip
-7za x main.zip -o%root%\Update
-del /s /q "%root%\Update\main.zip"
+7za x main.zip -o%ProgramData%\PhoenixOS\Update
+del /s /q "%ProgramData%\PhoenixOS\Update\main.zip"
 wget https://github.com/MrR736/Core/archive/refs/heads/main.zip
-7za x main.zip -o%root%\Update
-copy "%core%\CoreControls\Txt\del.txt" "%root%\Update"
+7za x main.zip -o%ProgramData%\PhoenixOS\Update
+copy "%ProgramData%\PhoenixOS\Core\CoreControls\Txt\del.txt" "%ProgramData%\PhoenixOS\Update"
 type del.txt >> core.cmd
 start core.cmd
 exit
